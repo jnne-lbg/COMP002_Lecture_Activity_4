772f22d8f2bcbfb8ead6bdd4ce48b0c2a3447b50
jeanne <jeannelabuga@gmail.com>
2025-10-29 18:48:29 -0700

    added 4 functions and explanation for function #3

--- script.js
+++ script.js
@@ -21,3 +21,17 @@ function function2(){
 }
 
 function1();
+
+/* 
+function1 first prints Statement 1, then Statement 2 after calling
+function2, then Statement 3 in function1. This is because of the stack order of the call stack, where it goes down a level after
+calling function2, then returns to function1 after it is done calling function2.
+*/
+
+function stackOverflow() {
+    return stackOverflow2();
+}
+
+function stackOverflow2() {
+    return stackOverflow1();
+}
